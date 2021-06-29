f742ee57d172fda04f04b846bf96b294b86ca994
ncpalmie <palmieronathan@gmail.com>
2021-06-28 20:07:30 -0400

    Split PlantCard out into its own component

--- src/components/Plant.js
+++ src/components/Plant.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import { Box, Card, DropButton, CardHeader, Image } from "grommet";
+import PlantCard from "./PlantCard";
 import "../css/Plant.css";
 
 function Plant(props) {
   const [cardVisible, setCardVisible] = useState(true);
-  const gene_array = ["x", "w", "g", "y", "h"];
 
   return (
     <Box
@@ -23,64 +23,12 @@ function Plant(props) {
           onMouseLeave={() => setCardVisible(false)}
         >
           {cardVisible ? (
-            <Card
-              className="Plant-Card"
-              background="#C7CFA0"
-              onMouseEnter={() => setCardVisible(true)}
-              onClick={(e) => {
-                e.stopPropagation();
-              }}
-            >
-              <CardHeader
-                className="Plant-Card-Header"
-                pad="small"
-                justify="center"
-              >
-                {"Plant " + (props.id + 1)}
-              </CardHeader>
-              <Box
-                direction="row"
-                alignSelf="center"
-                gap="small"
-                margin={{ left: "small", right: "small", bottom: "small" }}
-              >
-                {props.genes.map((gene, index) => (
-                  <DropButton
-                    className="Gene-Button"
-                    pad="none"
-                    label={
-                      <Image
-                        className="Gene-Image"
-                        src={gene + "_gene.png"}
-                        margin="none"
-                        alt="gene"
-                      />
-                    }
-                    dropAlign={{ top: "bottom", right: "right" }}
-                    dropProps={{ plain: true }}
-                    dropContent={
-                      <Box
-                        className="Gene-Dropdown-Box"
-                        gap="xsmall"
-                        pad={{ top: "xsmall", bottom: "xsmall" }}
-                        background="dark-3"
-                        round="small"
-                      >
-                        {gene_array.map((gene_option) => (
-                          <Image
-                            className="Gene-Dropdown-Image"
-                            src={gene_option + "_gene.png"}
-                            onClick={() => {
-                              props.changeGene(props.id, index, gene_option);
-                            }}
-                          />
-                        ))}
-                      </Box>
-                    }
-                  />
-                ))}
-              </Box>
-            </Card>
+            <PlantCard
+              genes={props.genes}
+              setCardVisible={setCardVisible}
+              changeGene={props.changeGene}
+              id={props.id}
+            />
           ) : null}
         </Box>
       ) : (
---
+++ src/components/PlantCard.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { Box, Card, CardHeader, DropButton, Image } from "grommet";
+import "../css/PlantCard.css";
+
+function PlantCard(props) {
+  const gene_array = ["x", "w", "g", "y", "h"];
+
+  return (
+    <Card
+      className="Plant-Card"
+      background="#C7CFA0"
+      onMouseEnter={() => props.setCardVisible(true)}
+      onClick={(e) => {
+        e.stopPropagation();
+      }}
+    >
+      <CardHeader className="Plant-Card-Header" pad="small" justify="center">
+        {"Plant " + (props.id + 1)}
+      </CardHeader>
+      <Box
+        direction="row"
+        alignSelf="center"
+        gap="small"
+        margin={{ left: "small", right: "small", bottom: "small" }}
+      >
+        {props.genes.map((gene, index) => (
+          <DropButton
+            className="Gene-Button"
+            pad="none"
+            label={
+              <Image
+                className="Gene-Image"
+                src={gene + "_gene.png"}
+                margin="none"
+                alt="gene"
+              />
+            }
+            dropAlign={{ top: "bottom", right: "right" }}
+            dropProps={{ plain: true }}
+            dropContent={
+              <Box
+                className="Gene-Dropdown-Box"
+                gap="xsmall"
+                pad={{ top: "xsmall", bottom: "xsmall" }}
+                background="dark-3"
+                round="small"
+              >
+                {gene_array.map((gene_option) => (
+                  <Image
+                    className="Gene-Dropdown-Image"
+                    src={gene_option + "_gene.png"}
+                    onClick={() => {
+                      props.changeGene(props.id, index, gene_option);
+                    }}
+                  />
+                ))}
+              </Box>
+            }
+          />
+        ))}
+      </Box>
+    </Card>
+  );
+}
+
+export default PlantCard;
