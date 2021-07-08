29fc3415cd14335ed145615a2cc3151e5c65b3b0
ncpalmie <palmieronathan@gmail.com>
2021-07-08 16:54:50 -0400

    Added a card to the genetics page with a short rundown on rust genetics and warning for the 50/50 method.

--- src/components/GeneticsLayout.js
+++ src/components/GeneticsLayout.js
@@ -46,6 +46,12 @@ const generatePlantStates = () => {
 };
 
 function GeneticsLayout(props) {
+  var video_link = (
+    <a href={"https://www.youtube.com/watch?v=FMxIgxkDZRI"}>
+      {" "}
+      Tamura77's 50/50 Method Explanation and Reason for Failure{" "}
+    </a>
+  );
   const [plants, setPlants] = useState(generatePlantStates());
 
   const togglePlant = (id) => {
@@ -122,6 +128,64 @@ function GeneticsLayout(props) {
           changeGene={changeGene}
           simPlanter={true}
         />
+      </Box>
+      <Box className="Info-Card-Box">
+        <Card className="Info-Card" background="#C7CFA0" height="large">
+          <CardHeader
+            className="Info-Header"
+            alignSelf="center"
+            margin={{ top: "small" }}
+          >
+            {"Genetics Rundown"}
+          </CardHeader>
+          <CardBody
+            className="Planter-Card-Body"
+            alignSelf="center"
+            wrap={true}
+            pad="medium"
+          >
+            {"Rust genetics can be somewhat complex and don't have many " +
+              "available explanations online. This tool was created as a way" +
+              " to test many iterations of plants without needing to wait" +
+              " the required 1-3 hours for in-game plants to grow. Each gene " +
+              "has a different effect on plant qualities: (G) results in " +
+              "faster growth, (H) increases hardiness meaning resistance to " +
+              "non-ideal temperatures, (Y) increases plant yield, (W) " +
+              "increases plant water intake, and (X) does nothing. The (W)" +
+              " and (X) red genes are considered undesirable but have higher " +
+              " weights [1.0] compared to green genes [0.6]. When determining" +
+              " the resulting genes in a column plants consider only " +
+              " neighbor genes and ignore their own (unless there is exactly " +
+              "one neighbor in which case a plant's own genes are included)." +
+              " For a more in-depth explanation of genetics and " +
+              "cross-breeding, check out Tamura77 on YouTube who provides a" +
+              " litany of useful information and videos on this subject."}
+          </CardBody>
+          <CardHeader
+            className="Info-Header"
+            alignSelf="center"
+            margin={{ top: "large" }}
+          >
+            {"50/50 Warning"}
+          </CardHeader>
+          <CardBody
+            className="Planter-Card-Body"
+            alignSelf="center"
+            wrap={true}
+            pad="medium"
+          >
+            A common gene manipulation method in Rust is the 50/50 method where
+            a single gene is changed using this process:
+            {video_link}
+            This gene simulator does NOT account for the in-game determination
+            of a priority plant resulting in the 50/50 method to sometimes fail
+            unintuitively. This is because this simulator only determines which
+            genes are possible in each gene slot, and not what plants will
+            actually result from the cross-breed. For plants that follow the
+            needed 50/50 setup where only one gene is changed though the
+            simulation is accurate and will tell you if the 50/50 can work.
+          </CardBody>
+        </Card>
       </Box>
     </Box>
   );
